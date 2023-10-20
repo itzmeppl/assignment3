05a4c5f638d78d65af73d67428c3574bd2c31e19
Riddhima <60081089+itzmeppl@users.noreply.github.com>
2023-10-20 10:12:46 -0400

    Add files via upload

---
+++ q2_network.pl
@@ -0,0 +1,32 @@
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: 
+%%%%% NAME:
+%%%%% NAME:
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
+%
+
+%%%%% SECTION: networkSolve
+%%%%% Below, you should define rules for the predicate "solve", which takes in your list of
+%%%%% variables and finds an assignment for each variable which solves the problem.
+%%%%%
+%%%%% You should also define rules for the predicate "solve_and_print" which calls your
+%%%%% solve predicate and prints out the results in an easy to read, human readable format.
+%%%%% The predicate "solve_and_print" should take in no arguments
+
+
+solve_and_print :-
+
+
+
+%%%%% SECTION: trip
+%%%%% Below, you should define rules for the predicate "trip", which takes in 
+%%%%% two computer systems and returns the list of locations in between them.
+%%%%% The signature of this predicate is trip(From,To,Path)
+
---
+++ q3_terms.pl
@@ -0,0 +1,24 @@
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: 
+%%%%% NAME:
+%%%%% NAME:
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
+%
+
+%%%%% SECTION: appendT
+%%%%% Add your rules for appendT(Term1,Term2,Result) below
+
+
+%%%%% SECTION: list2term
+%%%%% Add your rules for list2term(List,Term) below
+
+
+%%%%% SECTION: flat
+%%%%% Add your rules for flat(Term,FlatTerm) below
