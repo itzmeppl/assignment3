cb5f6b78c23109b56a971940fb5909da0f4bea6e
Riddhima <60081089+itzmeppl@users.noreply.github.com>
2023-10-25 00:08:46 -0400

    Update q2_network.pl

--- q2_network.pl
+++ q2_network.pl
@@ -20,7 +20,6 @@
 %%%%% solve predicate and prints out the results in an easy to read, human readable format.
 %%%%% The predicate "solve_and_print" should take in no arguments
 
-
 link(out,1).
 link(1,out).
 link(1,2).
@@ -39,6 +38,7 @@ allDiff([]).
 allDiff([H|T]):- not member(H,T), allDiff(T).
 
 
+
 trip(Start, End, Path) :-
     trip(Start, End, [Start], Path, []).
 
@@ -55,12 +55,11 @@ trip(Start, End, Visited, Path, AccPath) :-
 
 
 sys(1). sys(2). sys(3). sys(4). sys(5). sys(6).
+%sys(S1), sys(S2), sys(S3), sys(S4), sys(S5), sys(S6), 
 
 
 
 solve([Elby,Kim, Osborne, Tsuji, Wolverton, Zickerman,Catarina,Lizzie,Mona,Anthony,Daniel,Jamie,Pricenet, Sysworld, Univmoose, Banana, Firstbank,Netvue]):-
-
-%sys(S1), sys(S2), sys(S3), sys(S4), sys(S5), sys(S6), 
 
 lastN = [sys(Elby), sys(Kim), sys(Osborne), sys(Tsuji), sys(Wolverton), sys(Zickerman)],
 firstN = [sys(Catarina),sys(Lizzie),sys(Mona),sys(Anthony),sys(Daniel),sys(Jamie)],
@@ -69,23 +68,21 @@ woman = [sys(Catarina),sys(Lizzie),sys(Mona),sys(Anthony)],
 man = [sys(Anthony),sys(Daniel),sys(Jamie)],
 
 
-trip(Lizzie, Osborne, P1), member(Pricenet, P1),
+trip(Lizzie, Osborne, P1), append(P1, Lizzie, Pi), member(Pricenet, Pi),
 
 link(Mona, Wolverton), member(Netvue, [Mona, Wolverton]),
 
-trip(Anthony, Jamie, P2), member(Elby, P2),
+trip(Anthony, Jamie, P2), append(P2, Anthony, Pii), member(Elby, Pii),
 
 link(Daniel, out), link(Daniel, Sysworld),
 
-member(Tsuji, woman), trip(Jamie, Tsuji, P3), member(Univmoose, P3),
+member(Tsuji, woman), trip(Jamie, Tsuji, P3), append(P3, Jamie, Piii), member(Univmoose, Piii),
 
 member(Banana, woman),
 
-trip(Kim, out, P4), member(Firstbank, P4),
+trip(Kim, out, P4), append(P4, Kim, Piv),member(Firstbank, Piv),
 
 link(Zickerman, Catarina), link(Zickerman, Netvue),
 
 
 allDiff(lastN), allDiff(firstN), allDiff(net_addr), allDiff(woman), allDiff(man).
-
-
